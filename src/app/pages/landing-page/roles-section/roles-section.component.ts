230fffb95f76528313024a5584aad563be94beea
luca <lucajoglidze@gmail.com>
2023-07-15 02:18:29 +0400

    add animations

--- src/app/pages/landing-page/roles-section/roles-section.component.ts
+++ src/app/pages/landing-page/roles-section/roles-section.component.ts
@@ -1,15 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as Aos from 'aos';
 
 @Component({
   selector: 'app-roles-section',
   templateUrl: './roles-section.component.html',
   styleUrls: ['./roles-section.component.scss'],
 })
-export class RolesSectionComponent {
+export class RolesSectionComponent implements OnInit {
   isExpanded: boolean = true;
   isExpanded1: boolean = true;
   isExpanded2: boolean = true;
-
+  ngOnInit(): void {
+    Aos.init();
+  }
   expanded() {
     this.isExpanded = !this.isExpanded;
   }
