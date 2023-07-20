97d4bfc8296e79ea7fb0c65d1383977d06b0457b
Luca Joglidze <94470067+joglidze@users.noreply.github.com>
2023-07-20 17:04:12 +0400

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://task-manager-api-production-7f1c.up.railway.app/api/',
+  apiUrl: 'http://task-manager-api-ochre.vercel.app/api/',
 };
